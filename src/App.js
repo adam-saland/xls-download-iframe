e360325bd395a6a28bb60316c9509f1c40803920
adam-saland <adam.saland@openfin.co>
2021-04-08 08:58:58 -0400

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import XLSX from 'xlsx';
 import logo from './logo.svg';
 import './App.css';
 
+function exportToExcel(e) {
+  const data = [
+    ['id', 'name'],
+    ['1', 'juan'],
+    ['2', 'jose'],
+    ['3', 'adam'],
+  ]
+
+  const sheetName = 'sheet1'
+  const fileName = `myExcelFle-${Date.now().toString()}.xlsx`
+  const ws = XLSX.utils.aoa_to_sheet(data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, sheetName);
+  const file = XLSX.writeFile(wb, fileName)
+
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Export to excel - child app</p>
+        <button onClick={exportToExcel}>download</button>
       </header>
     </div>
   );
